ca2842dac9909c73421223f2a37debf7115faba7
Andriy Yanko <andriy.yanko@gmail.com>
2022-12-15 19:53:45 +0200

    Add email from template support

--- src/__tests__/mailtrap_client.test.ts
+++ src/__tests__/mailtrap_client.test.ts
@@ -61,4 +61,54 @@ describe("MailtrapClient#send", () => {
       expect(result).toEqual(successData);
     });
   });
+
+  describe("template", () => {
+    it("sends request to mailtrap api", async () => {
+      const successData = {
+        success: "true",
+        message_ids: ["00000000-00000000-00000000-00000001"],
+      };
+
+      mock
+        .onPost("https://send.api.mailtrap.io/api/send")
+        .reply(200, successData);
+
+      const client = new MailtrapClient({ token: "MY_API_TOKEN" });
+      const result = await client.send({
+        from: {
+          name: "Mailtrap",
+          email: "sender@mailtrap.io",
+        },
+        to: [
+          {
+            email: "recipient@mailtrap.io",
+          },
+        ],
+        template_uuid: "813e39db-c74a-4830-b037-0e6ba8b1fe88",
+        template_variables: {
+          user_name: "John Doe",
+        },
+      });
+
+      expect(mock.history.post[0].baseURL).toEqual(
+        "https://send.api.mailtrap.io"
+      );
+      expect(mock.history.post[0].url).toEqual("/api/send");
+      expect(mock.history.post[0].headers).toEqual({
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+        Authorization: "Bearer MY_API_TOKEN",
+        Connection: "keep-alive",
+      });
+      expect(mock.history.post[0].data).toEqual(
+        "{" +
+          '"from":{"name":"Mailtrap","email":"sender@mailtrap.io"},' +
+          '"to":[{"email":"recipient@mailtrap.io"}],' +
+          '"template_uuid":"813e39db-c74a-4830-b037-0e6ba8b1fe88",' +
+          '"template_variables":{"user_name":"John Doe"}' +
+          "}"
+      );
+      expect(result).toEqual(successData);
+    });
+  });
 });
