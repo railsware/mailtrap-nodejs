c47f54e1b11e598fae4aa47cf4f2b907a31b34fb
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-08-15 18:26:23 +0400

    config: move static vars.

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+export default {
+  ERRORS: {
+    FILENAME_REQUIRED: "Filename is required.",
+    CONTENT_REQUIRED: "Content is required.",
+    SUBJECT_REQUIRED: "Subject is required.",
+    FROM_REQUIRED: "From is required.",
+    INVALID_MAIL: "Mail is invalid.",
+    SENDING_FAILED: "Sending failed.",
+    NO_DATA_ERROR: "No Data.",
+  },
+  MAILTRAP_ENDPOINT: "https://send.api.mailtrap.io",
+  TRANSPORT_NAME: "MailtrapTransport",
+  USER_AGENT: "mailtrap-nodejs (https://github.com/railsware/mailtrap-nodejs)",
+};
