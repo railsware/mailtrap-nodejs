6d34215076f4c1a657fb1c47ff255ebd42657020
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-08-18 14:22:05 +0400

    lib: init nodemailer transport.

---
+++ src/lib/transport.ts
@@ -0,0 +1,64 @@
+import { Transport } from "nodemailer";
+import MailMessage from "nodemailer/lib/mailer/mail-message";
+
+import MailtrapClient from "./mailtrap-client";
+import normalizeCallback from "./normalizer";
+
+import CONFIG from "../config";
+
+import { MailtrapClientConfig } from "../types/mailtrap";
+import {
+  MailtrapResponse,
+  MailtrapTransporter,
+  NormalizeCallback,
+} from "../types/transporter";
+
+const { TRANSPORT_SETTINGS } = CONFIG;
+const { NAME } = TRANSPORT_SETTINGS;
+
+/**
+ * Avoid TypeScript treating project root as dist root.
+ */
+const packageData = require("../../package.json");
+
+/**
+ * Mailtrap transport for NodeMailer.
+ */
+class MailtrapTransport implements Transport<MailtrapResponse> {
+  public name: string;
+
+  public version: string;
+
+  private client: MailtrapClient;
+
+  /**
+   * Initialize transport `name`, `version` and `client`.
+   */
+  constructor(options: MailtrapClientConfig) {
+    this.name = NAME;
+    this.version = packageData.version;
+    this.client = new MailtrapClient(options);
+  }
+
+  /**
+   * Send message via `Mailtrap` client.
+   */
+  public send(
+    nodemailerMessage: MailMessage<MailtrapResponse>,
+    callback: NormalizeCallback
+  ): void {
+    nodemailerMessage.normalize(normalizeCallback(this.client, callback));
+  }
+}
+
+/**
+ * Extends `nodemailer` types to support Mailtrap API params.
+ */
+declare module "nodemailer" {
+  export function createTransport(
+    transport: MailtrapTransport
+  ): MailtrapTransporter;
+}
+
+export default (options: MailtrapClientConfig) =>
+  new MailtrapTransport(options);
