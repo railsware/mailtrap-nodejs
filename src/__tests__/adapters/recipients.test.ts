ef9fae95f0569605a8de0c3f641e47207240ef3f
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-08-22 22:35:41 +0400

    test: init adapters.

---
+++ src/__tests__/adapters/recipients.test.ts
@@ -0,0 +1,88 @@
+import adaptRecipients, {
+  adaptSingleRecipient,
+} from "../../adapters/recipients";
+
+describe("adapters/recipients: ", () => {
+  describe("adaptSingleRecipient(): ", () => {
+    it("builds object containing recipient email if recipient is string.", () => {
+      const recipient = "mock-recipient";
+
+      const expectedResult = {
+        email: recipient,
+      };
+      const result = adaptSingleRecipient(recipient);
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it("builds object containing name, email keys if recipient is Nodemailer address.", () => {
+      const recipient = {
+        name: "mock-name",
+        address: "mock-email",
+      };
+
+      const expectedResult = {
+        name: recipient.name,
+        email: recipient.address,
+      };
+      const result = adaptSingleRecipient(recipient);
+
+      expect(result).toEqual(expectedResult);
+    });
+  });
+
+  describe("adaptRecipients(): ", () => {
+    it("returns empty array if recipients is invalid.", () => {
+      const recipients = undefined;
+
+      const expectedResult: any = [];
+      const result = adaptRecipients(recipients);
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it("wraps adapted recipient into array if it's not an array.", () => {
+      const recipients = {
+        name: "mock-name",
+        address: "mock-email",
+      };
+
+      const expectedResult = [
+        {
+          name: recipients.name,
+          email: recipients.address,
+        },
+      ];
+      const result = adaptRecipients(recipients);
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it("returns adapted recipients array.", () => {
+      const recipients = [
+        {
+          name: "mock-name-1",
+          address: "mock-email-1",
+        },
+        {
+          name: "mock-name-2",
+          address: "mock-email-2",
+        },
+      ];
+
+      const expectedResult = [
+        {
+          name: recipients[0].name,
+          email: recipients[0].address,
+        },
+        {
+          name: recipients[1].name,
+          email: recipients[1].address,
+        },
+      ];
+      const result = adaptRecipients(recipients);
+
+      expect(result).toEqual(expectedResult);
+    });
+  });
+});
