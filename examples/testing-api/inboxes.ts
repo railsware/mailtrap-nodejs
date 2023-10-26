d4b1b137664b25023b15acc8cbf3f8c2fca057ad
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-10-27 00:36:04 +0400

    examples: init inboxes api usage.

---
+++ examples/testing-api/inboxes.ts
@@ -0,0 +1,35 @@
+import { MailtrapClient } from "../..//src"
+
+const TOKEN = "<YOUR-TOKEN-HERE>";
+const TEST_INBOX_ID = "<YOUR-TEST-INBOX-ID-HERE>"
+const ACCOUNT_ID = "<YOUR-ACCOUNT-ID-HERE>"
+
+const client = new MailtrapClient({ token: TOKEN, testInboxId: TEST_INBOX_ID, accountId: ACCOUNT_ID });
+
+const projectsClient = client.testing.projects
+const inboxesClient = client.testing.inboxes
+
+projectsClient.getList()
+  .then(async (projects) => {
+    if (projects && projects.length > 0) {
+      const firstProjectId = projects[0].id
+
+      // const inbox = await inboxesClient.create(firstProjectId, 'test-inbox')
+
+      const inboxes = await inboxesClient.getList()
+
+      if (inboxes && inboxes.length > 0) {
+        const firstInboxId = inboxes[0].id
+
+        const inboxAttributes = await inboxesClient.getInboxAttributes(firstInboxId)
+        await inboxesClient.updateInbox(firstInboxId, {name: 'mock-name', emailUsername: 'mocker'})
+        await inboxesClient.cleanInbox(firstInboxId)
+        await inboxesClient.markAsRead(firstInboxId)
+        await inboxesClient.resetCredentials(firstInboxId)
+        await inboxesClient.enableEmailAddress(firstInboxId)
+        const response = await inboxesClient.resetEmailAddress(firstInboxId)
+        
+        console.log(response)
+      }
+    }
+  })
