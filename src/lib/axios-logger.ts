50483282f9ece92737de56774dee155b56056bc6
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-10-27 00:34:07 +0400

    lib: tune axios logger to handle more errors  scenarios.

--- src/lib/axios-logger.ts
+++ src/lib/axios-logger.ts
@@ -2,6 +2,15 @@ import axios, { AxiosError } from "axios";
 
 import MailtrapError from "./MailtrapError";
 
+import { AxiosErrorObject } from "../types/axios";
+
+/**
+ * Axios error names guard.
+ */
+const hasErrorNames = (obj: unknown): obj is AxiosErrorObject =>
+  (obj as AxiosErrorObject)?.name !== undefined &&
+  Array.isArray((obj as AxiosErrorObject).name);
+
 /**
  * Error handler for axios response.
  */
@@ -13,22 +22,11 @@ export default function handleSendingError(error: AxiosError | unknown) {
       "errors" in error.response.data &&
       error.response.data.errors;
 
-    type AxiosErrorObject = {
-      name: string[];
-    };
-
-    const hasErrorNames = (obj: unknown): obj is AxiosErrorObject => {
-      return (
-        (obj as AxiosErrorObject)?.name !== undefined &&
-        Array.isArray((obj as AxiosErrorObject).name)
-      );
-    };
-
     const errorNames = hasErrorNames(serverErrorsObject)
-      ? serverErrorsObject.name
-      : undefined;
+      ? serverErrorsObject.name.join(", ")
+      : serverErrorsObject;
 
-    const message = errorNames ? errorNames.join(", ") : error.message;
+    const message = errorNames || error.message;
 
     // @ts-expect-error weird typing around Error class, but it's tested to work
     throw new MailtrapError(message, { cause: error });
