1fa1c36b94887ceba0902dd36302481e8fd62d1b
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-10-27 00:34:38 +0400

    types: reuse inboxes types in projects.

--- src/types/api/projects.ts
+++ src/types/api/projects.ts
@@ -1,39 +1,8 @@
+import { Inbox } from "./inboxes";
+
 type ShareLinks = {
   admin: string;
   viewer: string;
-};
-
-type Permissions = {
-  can_read: boolean;
-  can_update: boolean;
-  can_destroy: boolean;
-  can_leave: boolean;
-};
-
-type Inbox = {
-  id: number;
-  name: string;
-  username: string;
-  password: string;
-  max_size: number;
-  status: string;
-  email_username: string;
-  email_username_enabled: boolean;
-  sent_messages_count: number;
-  forwarded_messages_count: number;
-  used: boolean;
-  forward_from_email_address: string;
-  project_id: number;
-  domain: string;
-  pop3_domain: string;
-  email_domain: string;
-  smtp_ports: number[];
-  pop3_ports: number[];
-  emails_count: number;
-  emails_unread_count: number;
-  last_message_sent_at: string;
-  max_message_size: number;
-  permissions: Permissions;
 };
 
 export type Project = {
