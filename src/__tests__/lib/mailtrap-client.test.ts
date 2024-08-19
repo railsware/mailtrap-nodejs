ce67277c835aa5f893b8c2ca3b98b4c18976a164
Narek Hovhannisyan <narhovhannisian@gmail.com>
2024-08-19 14:03:28 +0400

    test: cover new mailtrap client functionality with units

--- src/__tests__/lib/mailtrap-client.test.ts
+++ src/__tests__/lib/mailtrap-client.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable no-console */
 import axios, { AxiosError } from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
@@ -6,9 +7,13 @@ import { Mail, MailtrapClient } from "../..";
 import MailtrapError from "../../lib/MailtrapError";
 
 import CONFIG from "../../config";
+import TestingAPI from "../../lib/api/Testing";
+import GeneralAPI from "../../lib/api/General";
 
-const { ERRORS } = CONFIG;
-const { TEST_INBOX_ID_MISSING, ACCOUNT_ID_MISSING } = ERRORS;
+const { ERRORS, CLIENT_SETTINGS } = CONFIG;
+const { TESTING_ENDPOINT, BULK_ENDPOINT, SENDING_ENDPOINT } = CLIENT_SETTINGS;
+const { TEST_INBOX_ID_MISSING, ACCOUNT_ID_MISSING, BULK_SANDBOX_INCOMPATIBLE } =
+  ERRORS;
 
 describe("lib/mailtrap-client: ", () => {
   let mock: AxiosMockAdapter;
@@ -27,31 +32,206 @@ describe("lib/mailtrap-client: ", () => {
     text: "My TEXT",
   };
 
-  beforeAll(() => {
-    mock = new AxiosMockAdapter(axios);
-  });
+  // beforeAll(() => {
+  //   mock = new AxiosMockAdapter(axios);
+  // });
 
-  afterEach(() => {
-    mock.reset();
-  });
+  // afterEach(() => {
+  //   mock.reset();
+  // });
 
   describe("send():", () => {
+    beforeAll(() => {
+      mock = new AxiosMockAdapter(axios);
+    });
+
+    afterEach(() => {
+      mock.reset();
+    });
+
+    it("rejects with Mailtrap error, bulk and sanbox modes are incompatible.", async () => {
+      const client = new MailtrapClient({
+        token: "MY_API_TOKEN",
+        bulk: true,
+        sandbox: true,
+      });
+
+      try {
+        await client.send(goodMail);
+      } catch (error) {
+        expect(error).toEqual(new MailtrapError(BULK_SANDBOX_INCOMPATIBLE));
+      }
+    });
+    const testInboxId = 100;
+
+    it("successfully sends testing email.", async () => {
+      const testingClient = new MailtrapClient({
+        token: "MY_API_TOKEN",
+        sandbox: true,
+        testInboxId,
+      });
+      const endpoint = `${TESTING_ENDPOINT}/api/send/${testInboxId}`;
+      const expectedResponseData = {
+        success: true,
+        message_ids: ["0c7fd939-02cf-11ed-88c2-0a58a9feac02"],
+      };
+      mock.onPost(endpoint).reply(200, expectedResponseData);
+
+      const emailData = {
+        from: {
+          email: "sender.mock@email.com",
+          name: "sender",
+        },
+        to: [
+          {
+            email: "recipient.mock@email.com",
+            name: "recipient",
+          },
+        ],
+        subject: "mock-subject",
+        text: "Mock text",
+        html: "<div>Mock text</div>",
+      };
+
+      const result = await testingClient.send(emailData);
+
+      expect(mock.history.post[0].url).toEqual(endpoint);
+      expect(mock.history.post[0].data).toEqual(JSON.stringify(emailData));
+      expect(result).toEqual(expectedResponseData);
+    });
+
+    it("successfully sends bulk email.", async () => {
+      const bulkClient = new MailtrapClient({
+        token: "MY_API_TOKEN",
+        bulk: true,
+      });
+      const endpoint = `${BULK_ENDPOINT}/api/send`;
+      const expectedResponseData = {
+        success: true,
+        message_ids: ["0c7fd939-02cf-11ed-88c2-0a58a9feac02"],
+      };
+      mock.onPost(endpoint).reply(200, expectedResponseData);
+
+      const emailData = {
+        from: {
+          email: "sender.mock@email.com",
+          name: "sender",
+        },
+        to: [
+          {
+            email: "recipient.mock@email.com",
+            name: "recipient",
+          },
+        ],
+        subject: "mock-subject",
+        text: "Mock text",
+        html: "<div>Mock text</div>",
+      };
+
+      const result = await bulkClient.send(emailData);
+
+      expect(mock.history.post[0].url).toEqual(endpoint);
+      expect(mock.history.post[0].data).toEqual(JSON.stringify(emailData));
+      expect(result).toEqual(expectedResponseData);
+    });
+
+    it("handles an API error.", async () => {
+      const testingClient = new MailtrapClient({
+        token: "MY_API_TOKEN",
+        sandbox: true,
+        testInboxId,
+      });
+      const responseData = {
+        success: false,
+        errors: ["mock-error-1", "mock-error-2"],
+      };
+
+      const endpoint = `${TESTING_ENDPOINT}/api/send/${testInboxId}`;
+
+      mock.onPost(endpoint).reply(400, responseData);
+
+      const emailData = {
+        from: {
+          email: "sender.mock@email.com",
+          name: "sender",
+        },
+        to: [
+          {
+            email: "recipient.mock@email.com",
+            name: "recipient",
+          },
+        ],
+        subject: "mock-subject",
+        text: "Mock text",
+        html: "<div>Mock text</div>",
+      };
+
+      const expectedErrorMessage = responseData.errors.join(",");
+
+      expect.assertions(3);
+
+      try {
+        await testingClient.send(emailData);
+      } catch (error) {
+        expect(mock.history.post[0].url).toEqual(endpoint);
+        expect(mock.history.post[0].data).toEqual(JSON.stringify(emailData));
+
+        if (error instanceof Error) {
+          expect(error.message).toEqual(expectedErrorMessage);
+        }
+      }
+    });
+
+    it("handles an HTTP transport error.", async () => {
+      const testingClient = new MailtrapClient({
+        token: "MY_API_TOKEN",
+        sandbox: true,
+        testInboxId,
+      });
+      const emailData = {
+        from: {
+          email: "sender.mock@email.com",
+          name: "sender",
+        },
+        to: [
+          {
+            email: "recipient.mock@email.com",
+            name: "recipient",
+          },
+        ],
+        subject: "mock-subject",
+        text: "Mock text",
+        html: "<div>Mock text</div>",
+      };
+
+      const expectedErrorMessage = "Request failed with status code 404";
+
+      expect.assertions(2);
+
+      try {
+        await testingClient.send(emailData);
+      } catch (error) {
+        expect(error).toBeInstanceOf(MailtrapError);
+
+        if (error instanceof MailtrapError) {
+          expect(error.message).toEqual(expectedErrorMessage);
+        }
+      }
+    });
+
     it("sends request to mailtrap api", async () => {
       const successData = {
         success: "true",
         message_ids: ["00000000-00000000-00000000-00000001"],
       };
+      const endpoint = `${SENDING_ENDPOINT}/api/send`;
 
-      mock
-        .onPost("https://send.api.mailtrap.io/api/send")
-        .reply(200, successData);
+      mock.onPost(endpoint).reply(200, successData);
 
       const client = new MailtrapClient({ token: "MY_API_TOKEN" });
       const result = await client.send(goodMail);
 
-      expect(mock.history.post[0].url).toEqual(
-        "https://send.api.mailtrap.io/api/send"
-      );
+      expect(mock.history.post[0].url).toEqual(endpoint);
       expect(mock.history.post[0].headers).toMatchObject({
         Accept: "application/json, text/plain, */*",
         "Content-Type": "application/json",
@@ -158,44 +338,58 @@ describe("lib/mailtrap-client: ", () => {
   });
 
   describe("get testing(): ", () => {
-    it("returns testing API object, console warn is called twice.", () => {
-      const originalWarn = console.warn;
-      const mockLogger = jest.fn();
-      console.warn = mockLogger;
-      const client = new MailtrapClient({ token: "MY_API_TOKEN" });
-
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const testingAPI = client.testing;
-
-      expect.assertions(3);
-
-      expect(mockLogger).toBeCalledTimes(2);
-      expect(mockLogger).toBeCalledWith(TEST_INBOX_ID_MISSING);
-      expect(mockLogger).toBeCalledWith(ACCOUNT_ID_MISSING);
-
-      console.warn = originalWarn;
-    });
-
-    it("return testing API object, without calling console warn.", () => {
-      const originalWarn = console.warn;
-      const mockLogger = jest.fn();
-      console.warn = mockLogger;
-      const testInboxId = 1;
-      const accountId = 1;
+    it("rejects with Mailtrap error, when `testInboxId` is missing.", () => {
       const client = new MailtrapClient({
         token: "MY_API_TOKEN",
-        testInboxId,
-        accountId,
       });
-
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const testingAPI = client.testing;
 
       expect.assertions(1);
 
-      expect(mockLogger).toBeCalledTimes(0);
+      try {
+        client.testing;
+      } catch (error) {
+        expect(error).toEqual(new MailtrapError(TEST_INBOX_ID_MISSING));
+      }
+    });
 
-      console.warn = originalWarn;
+    it("rejects with Mailtrap error, when `accountId` is missing.", () => {
+      const client = new MailtrapClient({
+        token: "MY_API_TOKEN",
+        testInboxId: 5,
+      });
+
+      expect.assertions(1);
+
+      try {
+        client.testing;
+      } catch (error) {
+        expect(error).toEqual(new MailtrapError(ACCOUNT_ID_MISSING));
+      }
+    });
+
+    it("returns testing API object, console warn is called twice.", () => {
+      const client = new MailtrapClient({
+        token: "MY_API_TOKEN",
+        sandbox: true,
+        testInboxId: 10,
+        accountId: 10,
+      });
+      expect.assertions(1);
+
+      const testingClient = client.testing;
+      expect(testingClient).toBeInstanceOf(TestingAPI);
+    });
+
+    describe("get general(): ", () => {
+      it("returns testing API object, console warn is called twice.", () => {
+        const client = new MailtrapClient({
+          token: "MY_API_TOKEN",
+        });
+        expect.assertions(1);
+
+        const generalClient = client.general;
+        expect(generalClient).toBeInstanceOf(GeneralAPI);
+      });
     });
   });
 });
