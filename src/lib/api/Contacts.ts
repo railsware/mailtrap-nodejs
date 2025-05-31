ad406400fe663c29563243412a685355aaa81558
Narek Hovhannisyan <narhovhannisian@gmail.com>
2025-05-31 16:40:22 +0400

    api: simplify ContactsBaseAPI by directly exposing ContactsApi methods for better usability

--- src/lib/api/Contacts.ts
+++ src/lib/api/Contacts.ts
@@ -1,21 +1,27 @@
 import { AxiosInstance } from "axios";
 
 import ContactsApi from "./resources/Contacts";
-import ContactListsApi from "./resources/ContactLists";
 
 export default class ContactsBaseAPI {
   private client: AxiosInstance;
 
   private accountId?: number;
 
-  public contacts: ContactsApi;
+  public create: ContactsApi["create"];
 
-  public contactLists: ContactListsApi;
+  public update: ContactsApi["update"];
+
+  public delete: ContactsApi["delete"];
+
+  public list: ContactsApi["list"];
 
   constructor(client: AxiosInstance, accountId?: number) {
     this.client = client;
     this.accountId = accountId;
-    this.contacts = new ContactsApi(this.client, this.accountId);
-    this.contactLists = new ContactListsApi(this.client, this.accountId);
+    const contacts = new ContactsApi(this.client, this.accountId);
+    this.create = contacts.create;
+    this.update = contacts.update;
+    this.delete = contacts.delete;
+    this.list = contacts.list;
   }
 }
