fb3d521893faa0cb9b3a1f6dfb76f5d8ab4c12ee
Narek Hovhannisyan <narhovhannisian@gmail.com>
2025-05-31 16:39:51 +0400

    types: update Contact interface to include status and change timestamp types to number; enhance ContactData and add ContactResponse interfaces for improved API response handling

--- src/types/api/contacts.ts
+++ src/types/api/contacts.ts
@@ -1,16 +1,16 @@
-export interface Contact {
+interface Contact {
   id: string;
+  status: "subscribed" | "unsubscribed";
   email: string;
-  fields: Record<string, string>;
+  fields: Record<string, string | number>;
   list_ids: number[];
-  unsubscribed: boolean;
-  created_at: string;
-  updated_at: string;
+  created_at: number;
+  updated_at: number;
 }
 
 export interface ContactData {
   email: string;
-  fields?: Record<string, string>;
+  fields?: Record<string, string | number>;
   list_ids?: number[];
   unsubscribed?: boolean;
 }
@@ -18,4 +18,21 @@ export interface ContactData {
 export interface ContactUpdateData extends ContactData {
   list_ids_included?: number[];
   list_ids_excluded?: number[];
+}
+
+export interface ContactResponse {
+  action: "updated" | "created" | "deleted";
+  data: Contact;
+}
+
+export interface CreateContactResponse {
+  data: {
+    id: string;
+    status: "subscribed" | "unsubscribed";
+    email: string;
+    fields: Record<string, string>;
+    list_ids: number[];
+    created_at: number;
+    updated_at: number;
+  };
 }
