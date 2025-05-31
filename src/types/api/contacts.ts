36dc156af7d3763257561c3e076993fdfa0c5d63
Narek Hovhannisyan <narhovhannisian@gmail.com>
2025-05-31 18:08:47 +0400

    types: refactor Contact interface to export it and reorder properties for clarity; update ContactData fields type for consistency

--- src/types/api/contacts.ts
+++ src/types/api/contacts.ts
@@ -1,16 +1,16 @@
-interface Contact {
+export interface Contact {
   id: string;
-  status: "subscribed" | "unsubscribed";
   email: string;
-  fields: Record<string, string | number>;
-  list_ids: number[];
   created_at: number;
   updated_at: number;
+  list_ids: number[];
+  status: "subscribed" | "unsubscribed";
+  fields: Record<string, string>;
 }
 
 export interface ContactData {
   email: string;
-  fields?: Record<string, string | number>;
+  fields?: Record<string, string>;
   list_ids?: number[];
   unsubscribed?: boolean;
 }
@@ -21,18 +21,5 @@ export interface ContactUpdateData extends ContactData {
 }
 
 export interface ContactResponse {
-  action: "updated" | "created" | "deleted";
   data: Contact;
-}
-
-export interface CreateContactResponse {
-  data: {
-    id: string;
-    status: "subscribed" | "unsubscribed";
-    email: string;
-    fields: Record<string, string>;
-    list_ids: number[];
-    created_at: number;
-    updated_at: number;
-  };
 }
