8d00c4d37d544b6fec83b9da2ffaee8dd7de8d5a
Narek Hovhannisyan <narhovhannisian@gmail.com>
2025-10-06 19:49:45 +0400

    api: add ContactFieldsBaseAPI for managing contact fields operations

---
+++ src/lib/api/ContactFields.ts
@@ -0,0 +1,30 @@
+import { AxiosInstance } from "axios";
+
+import ContactFieldsApi from "./resources/ContactFields";
+
+export default class ContactFieldsBaseAPI {
+  private client: AxiosInstance;
+
+  private accountId?: number;
+
+  public create: ContactFieldsApi["create"];
+
+  public get: ContactFieldsApi["get"];
+
+  public getList: ContactFieldsApi["getList"];
+
+  public update: ContactFieldsApi["update"];
+
+  public delete: ContactFieldsApi["delete"];
+
+  constructor(client: AxiosInstance, accountId?: number) {
+    this.client = client;
+    this.accountId = accountId;
+    const contactFields = new ContactFieldsApi(this.client, this.accountId);
+    this.create = contactFields.create.bind(contactFields);
+    this.get = contactFields.get.bind(contactFields);
+    this.getList = contactFields.getList.bind(contactFields);
+    this.update = contactFields.update.bind(contactFields);
+    this.delete = contactFields.delete.bind(contactFields);
+  }
+}
