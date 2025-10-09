f5e1be12aae1dcf20e399c80f389e51c3396f6a5
Narek Hovhannisyan <narhovhannisian@gmail.com>
2025-10-09 19:41:02 +0400

    types: add TypeScript interfaces for sending domains and DNS records

---
+++ src/types/api/sending-domains.ts
@@ -0,0 +1,40 @@
+export interface DnsRecord {
+  key: string;
+  domain: string;
+  type: string;
+  value: string;
+  status: string;
+  name: string;
+}
+
+export interface SendingDomainPermissions {
+  can_read: boolean;
+  can_update: boolean;
+  can_destroy: boolean;
+}
+
+export interface SendingDomain {
+  id: number;
+  domain_name: string;
+  demo: boolean;
+  compliance_status: string;
+  dns_verified: boolean;
+  dns_verified_at: string;
+  dns_records: DnsRecord[];
+  open_tracking_enabled: boolean;
+  click_tracking_enabled: boolean;
+  auto_unsubscribe_link_enabled: boolean;
+  custom_domain_tracking_enabled: boolean;
+  health_alerts_enabled: boolean;
+  critical_alerts_enabled: boolean;
+  alert_recipient_email: string;
+  permissions: SendingDomainPermissions;
+}
+
+export interface CreateSendingDomainParams {
+  domain_name: string;
+}
+
+export interface SetupInstructionsResponse {
+  message: string;
+}
