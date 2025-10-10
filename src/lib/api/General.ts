9f6e4b0f29d01101e22ba78be2d984c51bbc0289
Narek Hovhannisyan <narhovhannisian@gmail.com>
2025-10-10 14:47:35 +0400

    refactor: enhance GeneralAPI to conditionally instantiate account-specific APIs based on accountId presence

--- src/lib/api/General.ts
+++ src/lib/api/General.ts
@@ -5,15 +5,58 @@ import AccountsApi from "./resources/Accounts";
 import PermissionsApi from "./resources/Permissions";
 
 export default class GeneralAPI {
-  public accountAccesses: AccountAccessesApi;
-
   public accounts: AccountsApi;
 
-  public permissions: PermissionsApi;
+  private client: AxiosInstance;
 
-  constructor(client: AxiosInstance, accountId: number) {
-    this.accountAccesses = new AccountAccessesApi(client, accountId);
+  private accountId: number | null = null;
+
+  private accountAccessesInstance: AccountAccessesApi | null = null;
+
+  private permissionsInstance: PermissionsApi | null = null;
+
+  constructor(client: AxiosInstance, accountId?: number) {
+    this.client = client;
+    this.accountId = accountId || null;
     this.accounts = new AccountsApi(client);
-    this.permissions = new PermissionsApi(client, accountId);
+
+    // Only instantiate account-specific APIs if accountId is provided
+    if (this.accountId !== null) {
+      this.accountAccessesInstance = new AccountAccessesApi(
+        client,
+        this.accountId
+      );
+      this.permissionsInstance = new PermissionsApi(client, this.accountId);
+    }
+  }
+
+  private checkAccountIdPresence(): number {
+    if (this.accountId === null) {
+      throw new Error(
+        "Account ID is required for this operation. Please provide accountId when creating GeneralAPI instance."
+      );
+    }
+    return this.accountId;
+  }
+
+  public get accountAccesses(): AccountAccessesApi {
+    if (this.accountAccessesInstance === null) {
+      const accountId = this.checkAccountIdPresence();
+      this.accountAccessesInstance = new AccountAccessesApi(
+        this.client,
+        accountId
+      );
+    }
+
+    return this.accountAccessesInstance;
+  }
+
+  public get permissions(): PermissionsApi {
+    if (this.permissionsInstance === null) {
+      const accountId = this.checkAccountIdPresence();
+      this.permissionsInstance = new PermissionsApi(this.client, accountId);
+    }
+
+    return this.permissionsInstance;
   }
 }
